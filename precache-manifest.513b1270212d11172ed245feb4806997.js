b2dea91ca963f08854d0b7389d198c986373a730
David Gibbons <gibbonsdav@gmail.com>
2020-03-28 14:15:21 -0700

    Updates

---
+++ precache-manifest.513b1270212d11172ed245feb4806997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4d92f1c7b58ca078e150a927796abc",
+    "url": "/photoalbum2/index.html"
+  },
+  {
+    "revision": "149bdb6d67f9f17b737d",
+    "url": "/photoalbum2/static/css/main.4e5a3cd3.chunk.css"
+  },
+  {
+    "revision": "5b682ddc6d81ce9d9f3d",
+    "url": "/photoalbum2/static/js/2.51576f2b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/photoalbum2/static/js/2.51576f2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "149bdb6d67f9f17b737d",
+    "url": "/photoalbum2/static/js/main.28cbf380.chunk.js"
+  },
+  {
+    "revision": "41519fe81cdcfbc77636",
+    "url": "/photoalbum2/static/js/runtime-main.5ec64ef9.js"
+  }
+]);
